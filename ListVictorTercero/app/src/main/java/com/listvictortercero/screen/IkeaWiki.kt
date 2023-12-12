6d9ac205b5b8435e47e12dab48a53e053e1f048c
Varixx0 <victermug@alu.edu.gva.es>
2023-12-12 09:49:40 +0100

    Acabado

--- ListVictorTercero/app/src/main/java/com/listvictortercero/screen/IkeaWiki.kt
+++ ListVictorTercero/app/src/main/java/com/listvictortercero/screen/IkeaWiki.kt
@@ -19,16 +19,16 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,12 +44,21 @@ fun IkeaWiki(
     ikeaViewModel: IkeaViewModel
 ){
     val furniture: Ikea by ikeaViewModel.selectedFurniture.observeAsState(initial = Ikea())
-    
+    var favorite by rememberSaveable {
+        mutableStateOf(0xFFFFFFFF)
+    }
     Column (modifier = Modifier
         .fillMaxWidth()
         .background(MaterialTheme.colorScheme.background)
         .padding(8.dp)
     ){
+        if (furniture.favorite){
+            favorite= 0xFFFFFFFF
+            furniture.favorite=false
+        } else{
+            favorite = 0xFFFFD700
+            furniture.favorite=true
+        }
         Row (
             modifier = Modifier.clickable {
                 navController.popBackStack()
@@ -72,11 +81,14 @@ fun IkeaWiki(
             Spacer(modifier = Modifier.width(30.dp))
             Button(onClick = {
                 if (furniture.favorite){
-
+                    favorite= 0xFFFFFFFF
+                    furniture.favorite=false
+                } else{
+                    favorite = 0xFFFFD700
+                    furniture.favorite=true
                 }
-
                              } , colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary)) {
-                Icon(imageVector = Icons.Default.Star, contentDescription ="", tint = Color.Black)
+                Icon(imageVector = Icons.Default.Star, contentDescription ="", tint = Color(favorite))
             }
         }
         Spacer(modifier = Modifier.height(20.dp))
